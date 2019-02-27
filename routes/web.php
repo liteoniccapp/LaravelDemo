44a8f36d0d8ea5f2dd011fce18343fc5502ca792
Allen Sun <tel3342311@gmail.com>
2019-02-27 16:40:46 +0800

    restore welcome

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,9 @@ use App\Post;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
 // Route::get('/', function() {
 
